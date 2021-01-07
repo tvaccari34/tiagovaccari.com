6df413650938892eaf8c0e48fadb0765537e0dbe
Tiago Vaccari <tiago.vaccari@outlook.com>
2021-01-07 19:14:41 +0000

    Google Analytics Key changed location

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = "G-CEMR345Y9E";
+export const GA_TRACKING_ID = process.env.GA_TRACKING_ID_KEY;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL): void => {
