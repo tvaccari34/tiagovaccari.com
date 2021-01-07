422504777f8a45950427ef1e034caf20fd324462
Tiago Vaccari <tiago.vaccari@outlook.com>
2021-01-07 15:36:44 +0000

    Profile page updated with new text

--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -8,9 +8,11 @@ const Profile: React.FC = ({children}) => {
                     <blockquote>"The only way to discover the limits of the possible is to go beyond them into the impossible."</blockquote>
                     <h5>Arthur C. Clarke</h5>
 
-                    <p>Developer, dad, husband, cyclist. 
-                        I'm passionate by code and technology.
-                        I started my tech life earlier 90's with my PC-286 and then 386 creating some funny programs. Now a days, have been working as Fullstack Web Developer.</p>
+                    <p>Developer, dad, husband, cyclist. I'm passionate by code and technology. I started my tech life earlier 90's with my PC-286 and then 386 creating some funny programs. Now a days, have been working as Fullstack Web Developer.</p>
+                    <p>I have more than 20 years of working experience. I started my career after got my first technical degree in 1995 working with Clipper and Visual Basic and then I got my Bachelor's Degree in Information System. I have played both technical roles and management/executive roles. I have been working for companies in different industries like Insurance Companies, Healthcare, and Technology Startups.</p>
+                    <p>I would say I don't have a specific technology to call "my love", but I have been working with Microsoft stack most of that time (C#, MS Sql, Asp.Net, Entity Framework). Recently, I'm working with the stack .Net Framework, AngularJs, TypeScript, MongoDB, MySql.</p>
+                    <p>In my spare time, I used to be with my family, friends or trying different flavors which can be a 🍺, 🍷 or a new tech to work with.</p> By the way, that blog was created using ReactJS, Next.js, TypeScript, Ghost CMS and Docker.
+                    <p>I born and raised in Jundiai-SP Brazil a lovely city with many italian roots. Now I'm living in Bournemouth UK which has the most beautiful beaches in UK, in my opinion 😃.</p>
                     <p>You can contact me on <a href='https://twitter.com/vaccari_tiago' target="_blank">Twitter</a>  </p>
                 </ProfileContent>
             </ProfileContainer>
