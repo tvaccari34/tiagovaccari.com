8ba31e95b0704e8a5ec3186fa9cca75137021177
Tiago Vaccari <tiago.vaccari@outlook.com>
2021-01-09 21:25:19 +0000

    Konami code added

--- pages/secretlab/index.tsx
+++ pages/secretlab/index.tsx
@@ -4,36 +4,63 @@ import { SecrectLabContainer, SecrectLabContent } from '../../styles/styles.secr
 import { FiChevronRight } from 'react-icons/fi'
 import Link from 'next/link';
 import Header from '../../components/Header';
+import { useState } from 'react';
     
 
-type Post = {
-    title: string;
-    slug: string;
-    custom_excerpt: string;
-  }
+// type Post = {
+//     title: string;
+//     slug: string;
+//     custom_excerpt: string;
+//   }
   
-  async function getPosts() {
+  // async function getPosts() {
   
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/ghost/api/v3/content/posts/?key=${process.env.CONTENT_API_KEY}&fields=title,slug,custom_excerpt`
-    ).then((res) => res.json());
+  //   const res = await fetch(
+  //     `${process.env.NEXT_PUBLIC_API_URL}/ghost/api/v3/content/posts/?key=${process.env.CONTENT_API_KEY}&fields=title,slug,custom_excerpt`
+  //   ).then((res) => res.json());
   
-    const posts = res.posts;
+  //   const posts = res.posts;
   
-    return posts;
-  }
+  //   return posts;
+  // }
   
-  export const getStaticProps: GetStaticProps = async ({ params }) => {
-    const posts = await getPosts();
-    return {
-      props: { posts },
-      revalidate: 60
+  // export const getStaticProps: GetStaticProps = async ({ params }) => {
+  //   const posts = await getPosts();
+  //   return {
+  //     props: { posts },
+  //     revalidate: 60
+  //   }
+  // }
+
+const SecrectLab: React.FC = () => {
+
+    const [isSecretDiscovered, setIsSecretDiscovered] = useState(false);
+
+    const konamiPattern = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+
+    let currentKey = 0;
+
+    const checkKey = (event) => {
+
+      if(konamiPattern.indexOf(event.key) < 0 || event.key !== konamiPattern[currentKey]){
+
+        currentKey = 0;
+        return;
+
+      }
+      
+      currentKey++;
+
+      if (konamiPattern.length === currentKey) {
+        currentKey = 0;
+
+        setIsSecretDiscovered(true);
+      }
     }
-  }
 
-const SecrectLab: React.FC<{ posts: Post[]}> = (props) => {
-
-    const {posts} = props;
+    if(process.browser){
+      document.addEventListener('keydown', checkKey, false);  
+    }
 
     return (
         <>
@@ -45,6 +72,22 @@ const SecrectLab: React.FC<{ posts: Post[]}> = (props) => {
                 <SecrectLabContent>
                 <h1>SHHHHHHH It is still a SECRECT!</h1>
                 </SecrectLabContent>
+
+                {
+                  isSecretDiscovered && (
+                    <>
+                      <img src="./images/lab.svg" alt="Secret Lab"/>
+                      <SecrectLabContent>
+                        <p><h3>You got the secret, so maybe you deserve to know more about software development! But just wait...</h3></p>
+                        <p><h3>You will need to prove your value! For that, you have to create a post about what you have learned during last week and tag me on Linkedin, Twitter, Instagram or Facebook.</h3></p>
+                        <div className="credits">
+                          <span>Credits - </span>
+                          <a href='https://www.freepik.com/vectors/technology'><span>Technology vector created by stories - www.freepik.com</span></a>
+                        </div>
+                      </SecrectLabContent>
+                    </>
+                  )
+                }
             </SecrectLabContainer>
         </>
     )
--- styles/styles.secretlab.ts
+++ styles/styles.secretlab.ts
@@ -5,16 +5,39 @@ export const SecrectLabContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    flex-direction: column;
     width: 100%;
 
     margin-top: 96px;
 
-    h1{
-        text-align: center;
-        color: #5BE7A9;
+    img{
+        width: 400px;
+        padding: 36px;
     }
+
+    
 `;
 
 export const SecrectLabContent = styled.div`
 
+    padding: 0 36px;
+
+    h1{
+        text-align: center;
+        color: #5BE7A9;
+        margin-bottom: 24px;
+    }
+
+    h3{
+        text-align: center;
+        color: #5BE7A9;
+        margin: 12px 96px;
+    }
+
+    
+    div.credits{
+        display: flex;
+        justify-content: flex-end;
+        font-size: 10px;
+    }
 `;
